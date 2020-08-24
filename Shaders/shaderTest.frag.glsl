0aeaa3c2218469720baa321eccb827920e63583b
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-08-24 16:57:22 -0400

    early beginnings

--- Shaders/shaderTest.frag.glsl
+++ Shaders/shaderTest.frag.glsl
@@ -4,17 +4,105 @@
 precision mediump float;
 #endif
 
-// uniform vec2 u_resolution;
+in vec4 gl_FragCoord;
+
+uniform vec2 u_resolution;
 // uniform vec2 u_mouse;
-// uniform float u_time;
+uniform float u_time;
 
 uniform float u_red;
 uniform float u_green;
 uniform float u_blue;
 
+uniform float u_lifetime;
+uniform vec2 u_velocity;
+uniform int u_particleShape;// Triangle = 0; Rect = 1; Circle = 2;
+uniform int u_emissionRotation;
+uniform vec2 u_sizeRange;
+uniform vec2 u_transparencyRange;
+
+// uniform int u_particleAmount;
+const int particleMax = 1024;
+uniform float  u_particles[particleMax];
+
 out vec4 fragColor;
 
-void main(){
+const float PHI = 1.61803398874989484820459; // Φ = Golden Ratio 
 
-	fragColor = vec4(u_red, u_green, u_blue, 1.0);
+float rand(vec2 xy,float seed)
+{
+    return fract(tan(distance(xy*PHI, xy)*seed)*xy.x);
+}
+
+float randRange(int mini, int maxi){
+	
+	return mod(round(rand(gl_FragCoord.xy / u_resolution.xy,u_time)),maxi-mini) + mini;
+}
+
+float Band(float t, float start, float end, float blur){
+	float sStep = smoothstep(start - blur,start + blur,t);
+	float eStep = smoothstep(end+blur, end - blur,t); 
+	return  sStep * eStep; 
+
+}
+
+float Circle(vec2 uv, vec2 pos,float radius, float blur ){
+	float d = length(uv-pos);
+	return smoothstep(radius,radius-blur,d);
+
+}
+
+float Rect(vec2 uv, vec2 pos, float size, float blur){
+	float band1 = Band(uv.x,pos.x,pos.x+size,blur);
+	float band2 = Band(uv.y,pos.y,pos.y+size,blur);
+
+	return band2 * band1;
+}
+
+// barycentric method
+float Triangle(vec2 uv, vec2 p0, vec2 p1,vec2 p2){
+	float s = p0.y * p2.x - p0.x * p2.y + (p2.y - p0.y) * uv.x + (p0.x - p2.x) * uv.y;
+    float t = p0.x * p1.y - p0.y * p1.x + (p0.y - p1.y) * uv.x + (p1.x - p0.x) * uv.y;
+	if(s < 0 != t < 0){
+		return .0;
+	}
+	 float A = -p1.y * p2.x + p0.y * (p2.x - p1.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y;;
+	 if(A < 0 && s <= 0 && s + t >= A){
+		 return 1.;
+	 }
+	 else if(s >= 0 && s + t <= A){
+		 return 1.;
+	 }
+	 return .0;
+}
+float Tri(vec2 uv, vec2 pos, float size){
+	vec2 right = pos;
+	right.x += size;
+	vec2 top = pos;
+	top.x += size * 0.5;
+	top.y += - size;
+	return Triangle(uv,pos,right,top);
+}
+
+void main(){
+	vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+
+
+	uv -= .5;
+	uv.x *= u_resolution.x / u_resolution.y;
+	
+	float delta = u_time / u_lifetime;
+	vec2 dir = u_velocity * delta;
+
+	if(u_particles.length() > 0)
+		dir += rand(uv,u_particles[0]);
+	vec3 col = vec3(u_red,u_green,u_blue)*Tri(uv,dir,.07);
+	for(int i = 0; i < u_particles.length();i++){
+		// if(u_particles[i] < u_lifetime){
+		// 	// dir += rand(uv,i);
+		// 	col *= Tri(uv,dir,.07);
+		// }
+	}
+		
+	fragColor = vec4(col, 1.0);
 }
--- Sources/Project.hx
+++ Sources/Project.hx
@@ -1,5 +1,7 @@
 package;
 
+import kha.arrays.Float32Array;
+import kha.graphics4.TextureUnit;
 import kha.Framebuffer;
 import kha.Color;
 import kha.Assets;
@@ -32,9 +34,21 @@ class Project {
 
 	var ui:Zui;
 
+	var velocity:FastVector2;
+
+	final maxParticles:Int;
+	var particles:Float32Array;
+	var particleAmount:Int;
+
 	var red:Float;
 	var green:Float;
 	var blue:Float;
+
+	var lifetimeID:ConstantLocation;
+	var velocityID:ConstantLocation;
+	
+	var particlesID:ConstantLocation;
+	var particleAmountID:ConstantLocation;
 
 	var redID:ConstantLocation;
 	var greenID:ConstantLocation;
@@ -48,23 +62,52 @@ class Project {
 		setupShader();
 
 		resolution = new FastVector2(Main.WIDTH, Main.HEIGHT);
+		velocity = new FastVector2(0.0,-10.0);
 		mouse = new FastVector2();
 
+		particleAmount = 1;
+		maxParticles = 1024;
+		particles = new Float32Array(maxParticles);
+		
 		Mouse.get().notify(onMouseDown, onMouseUp, onMouseMove, null);
 	}
 
+	var lastTime:Float;
+	var dt:Float;
 	public function update():Void {
-		
+		dt = Scheduler.realTime() - lastTime;
+		lastTime = Scheduler.realTime();
+		timeCount += dt;
+		for(i in 0...particleAmount){
+			particles[i] +=dt; 
+		}
+		if(timeCount > lifetimeHandle.value){
+			timeCount = 0.0;
+		}
 	}
 
+	var timeCount:Float;
 	public function render(framebuffer:Framebuffer):Void {
 		
 		backbuffer.g4.begin();
 		backbuffer.g4.setPipeline(pipeline);
 		
 		backbuffer.g4.setVector2(resolutionID, resolution);
-		backbuffer.g4.setVector2(mouseID, mouse);
-		backbuffer.g4.setFloat(timeID, Scheduler.time());
+		backbuffer.g4.setVector2(mouseID, mouse);		
+
+		backbuffer.g4.setFloat(timeID, timeCount);
+
+
+		backbuffer.g4.setInt(particleAmountID,particleAmount);
+		backbuffer.g4.setFloats(particlesID,particles);
+		
+		if(lifetimeHandle.changed){
+			backbuffer.g4.setFloat(lifetimeID, lifetimeHandle.value);
+		}
+
+		if(velocityChanged){
+			backbuffer.g4.setVector2(velocityID, velocity);
+		}
 
 		backbuffer.g4.setFloat(redID, red);
 		backbuffer.g4.setFloat(greenID, green);
@@ -117,18 +160,34 @@ class Project {
 		mouseID = pipeline.getConstantLocation('u_mouse');
 		timeID = pipeline.getConstantLocation('u_time');
 
+		lifetimeID = pipeline.getConstantLocation('u_lifetime');
+		velocityID = pipeline.getConstantLocation('u_velocity');
+
+		particlesID = pipeline.getConstantLocation("u_particles");
+		particleAmountID = pipeline.getConstantLocation("u_particleAmount");
+
 		redID = pipeline.getConstantLocation('u_red');
 		greenID = pipeline.getConstantLocation('u_green');
 		blueID = pipeline.getConstantLocation('u_blue');
 	}
 
+	var lifetimeHandle = Id.handle({value:1.0});
+	var velocityChanged:Bool = true;
 	public function gui(graphics:Graphics){
 		ui.begin(graphics);
-		if (ui.window(Id.handle(), 0, 0, 128, 128,true)){
-			if (ui.panel(Id.handle(), 'controls')){
-				red = ui.slider(Id.handle(), 'red', 0, 1, true, 100);
+		if (ui.window(Id.handle(), 0, 0, 128, 512,true)){
+			if (ui.panel(Id.handle({selected: true}), 'controls')){
+				red = ui.slider(Id.handle({value: 1.0}), 'red', 0, 1, true, 100);
 				green = ui.slider(Id.handle(), 'green', 0, 1, true, 100);
 				blue = ui.slider(Id.handle(), 'blue', 0, 1, true, 100);
+				ui.slider(lifetimeHandle,"lifetime",0.0,100,true);
+				var velXH = Id.handle({value: velocity.x});
+				var velYH = Id.handle({value: velocity.y});
+				ui.text("Velocity:");
+				ui.row([0.5,0.5]);
+				velocity.x = ui.slider(velXH,"X",-10.0,10.0,true,1/10)/10;
+				velocity.y = ui.slider(velYH,"Y",-10.0,10.0,true,1/10)/10;
+				velocityChanged = velXH.changed || velYH.changed;
 			}
 		}
 		ui.end();
